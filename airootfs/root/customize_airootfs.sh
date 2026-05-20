f1d40398bc66620f00d70bf4c168aacbff358aac
maironkk <luizfernandoferro154@gmail.com>
2026-05-20 03:43:40 -0400

    remover yay do customize_airootfs

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 set -e
 
-# Habilitar serviços
 systemctl enable NetworkManager
 systemctl enable sddm
 
-# Criar usuário
 useradd -m -G wheel,video,audio,input -s /bin/bash live
 echo "live:live" | chpasswd
 echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-
-# Instalar yay
-cd /tmp
-git clone https://aur.archlinux.org/yay.git
-cd yay
-sudo -u live makepkg -si --noconfirm
